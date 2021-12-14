cf5e9145aeb630b65d57165e3fa76681d8238826
goodtaeeun <goodtaeeun@kaist.ac.kr>
2021-12-14 18:15:02 +0900

    [Script] fix benchmark interface

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -9,83 +9,61 @@ BINUTILS_TARGETS = [
     ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGo", "original"),
     ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGo", "original"),
 
-    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT", ),
-    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT"),
-    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT"),
-    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT"),
-    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT"),
-    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT"),
+    # ini/par/eva/min = 1.0/0.8/0.9/0.1 (back to original)
+    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT", "9fa81a5"),
+    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT", "9fa81a5"),
+    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT", "9fa81a5"),
+    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT", "9fa81a5"),
+    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT", "9fa81a5"),
+    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT", "9fa81a5"),
 
-    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT"),
-    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT"),
-    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT"),
-    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT"),
-    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT"),
-    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT"),
+    # ini/par/eva/min = 1.0/0.8/0.9/0.2
+    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT", "e6d9cf2"),
+    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT", "e6d9cf2"),
+    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT", "e6d9cf2"),
+    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT", "e6d9cf2"),
+    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT", "e6d9cf2"),
+    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT", "e6d9cf2"),
 
-    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT"),
-    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT"),
-    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT"),
-    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT"),
-    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT"),
-    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT"),
+    # ini/par/eva/min = 1.0/0.8/0.95/0.1
+    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT", "860498e"),
+    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT", "860498e"),
+    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT", "860498e"),
+    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT", "860498e"),
+    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT", "860498e"),
+    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT", "860498e"),
 
-    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT"),
-    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT"),
-    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT"),
-    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT"),
-    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT"),
-    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT"),
+    # ini/par/eva/min = 1.0/0.8/0.8/0.1
+    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT", "e8e547f"),
+    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT", "e8e547f"),
+    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT", "e8e547f"),
+    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT", "e8e547f"),
+    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT", "e8e547f"),
+    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT", "e8e547f"),
 
-    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT"),
-    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT"),
-    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT"),
-    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT"),
-    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT"),
-    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT"),
+    # ini/par/eva/min = 1.0/0.95/0.9/0.1
+    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT", "9f61798"),
+    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT", "9f61798"),
+    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT", "9f61798"),
+    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT", "9f61798"),
+    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT", "9f61798"),
+    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT", "9f61798"),
 
-    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT"),
-    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT"),
-    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT"),
-    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT"),
-    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT"),
-    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT"),
+    # ini/par/eva/min = 1.0/0.9/0.9/0.1
+    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT", "8a60761"),
+    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT", "8a60761"),
+    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT", "8a60761"),
+    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT", "8a60761"),
+    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT", "8a60761"),
+    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT", "8a60761"),
 
-    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT"),
-    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT"),
-    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT"),
-    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT"),
-    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT"),
-    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT"),
-
-    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT"),
-    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT"),
-    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT"),
-    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT"),
-    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT"),
-    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT"),
-
-    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT"),
-    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT"),
-    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT"),
-    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT"),
-    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT"),
-    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT"),
-
-    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT"),
-    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT"),
-    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT"),
-    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT"),
-    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT"),
-    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT"),
-
-    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT"),
-    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT"),
-    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT"),
-    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT"),
-    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT"),
-    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT"),
-
+    # ini/par/eva/min = 0.8/0.8/0.9/0.1
+    ("cxxfilt-2016-4487", "stdin", check_cxxfilt_2016_4487, "AFLGOPT", "71c0cdb"),
+    ("cxxfilt-2016-4489", "stdin", check_cxxfilt_2016_4489, "AFLGOPT", "71c0cdb"),
+    ("cxxfilt-2016-4490", "stdin", check_cxxfilt_2016_4490, "AFLGOPT", "71c0cdb"),
+    ("cxxfilt-2016-4491", "stdin", check_cxxfilt_2016_4491, "AFLGOPT", "71c0cdb"),
+    ("cxxfilt-2016-4492", "stdin", check_cxxfilt_2016_4492, "AFLGOPT", "71c0cdb"),
+    ("cxxfilt-2016-6131", "stdin", check_cxxfilt_2016_6131, "AFLGOPT", "71c0cdb"),
 
 
 ]
@@ -94,14 +72,9 @@ BINUTILS_TARGETS = [
 ALL_TARGETS = BINUTILS_TARGETS
 
 
-def generate_worklist(benchmark, iteration):
-    if benchmark == "all":
-        targ_list = ALL_TARGETS
-    elif benchmark == "binutils":
-        targ_list = BINUTILS_TARGETS
-    else:
-        print("Unsupported benchmark: %s" % benchmark)
-        exit(1)
+def generate_worklist(iteration):
+    
+    targ_list = BINUTILS_TARGETS
 
     worklist = []
     for (targ_name, input_src, _, tool, version) in targ_list:
